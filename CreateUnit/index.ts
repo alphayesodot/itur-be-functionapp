c871a240a09ec724376de8456ef0dc59be3ec77b
CarmelTsadok <carmelcarmel88888@gmail.com>
2021-02-09 17:28:08 +0200

    feat: Add create unit function

---
+++ CreateUnit/index.ts
@@ -0,0 +1,25 @@
+import { AzureFunction, Context, HttpRequest } from '@azure/functions';
+import getConnection from '../shared/services/db';
+import FunctionError from '../shared/services/error';
+import { createUnitSchema } from '../shared/unit/unit.schema';
+import UnitModel from '../shared/unit/unit.model';
+
+const getResObject = (statusCode, errorMessage) => {
+    return { status: statusCode ?? process.env.SERVER_ERROR_CODE, body: errorMessage };
+};
+
+const httpTrigger: AzureFunction = async (context: Context, req: HttpRequest): Promise<void> => {
+    try {
+        await getConnection();
+
+        const { error } = createUnitSchema.validate(req);
+        if (error) throw new FunctionError(parseInt(process.env.VALIDATION_ERROR_CODE, 10), error.message);
+
+        const unit = await UnitModel.create(req.body);
+        context.res = getResObject(parseInt(process.env.SUCCESS_CODE, 10), unit);
+    } catch (err) {
+        context.res = getResObject(err.code, err.message);
+    }
+};
+
+export default httpTrigger;
--- shared/unit/unit.schema.ts
+++ shared/unit/unit.schema.ts
@@ -1,13 +1,24 @@
 import * as Joi from 'joi';
 
+const objectIdType = Joi.string().regex(/^[a-f\d]{24}$/i);
+
 export const getUnitByIdSchema = Joi.object({
     params: {
-        id: Joi.string().regex(/^[a-f\d]{24}$/i),
+        id: objectIdType,
     },
 }).unknown();
 
 export const deleteUnitByIdSchema = Joi.object({
     params: {
-        id: Joi.string().regex(/^[a-f\d]{24}$/i),
+        id: objectIdType,
+    },
+}).unknown();
+
+export const createUnitSchema = Joi.object({
+    body: {
+        name: Joi.string().required(),
+        owners: Joi.array().items(objectIdType).required(),
+        interviewers: Joi.array().items(objectIdType).required(),
+        nodes: Joi.array().items(Joi.string()).required(),
     },
 }).unknown();
