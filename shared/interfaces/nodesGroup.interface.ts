02249e99444f2077e0727a9a5881d2ea78a9378d
may kremer <maykremer@gmail.com>
2021-02-09 14:09:05 +0200

    initiate project

--- shared/interfaces/nodesGroup.interface.ts
+++ shared/interfaces/nodesGroup.interface.ts
@@ -1,6 +1,7 @@
 import { ObjectId } from 'mongoose';
 
 export default interface INodesGroup {
+    name: [string];
     owners: [ObjectId];
     interviewers: [ObjectId];
     nodes: [string];
--- shared/models/nodesGroup.model.ts
+++ shared/models/nodesGroup.model.ts
@@ -1,12 +1,13 @@
 import * as mongoose from 'mongoose';
 import INodesGroup from '../interfaces/nodesGroup.interface';
 
-const nodeGroupSchema = new mongoose.Schema({
+const nodesGroupSchema = new mongoose.Schema({
+    name: String,
     owners: [mongoose.Types.ObjectId],
     interviewers: [mongoose.Types.ObjectId],
     nodes: [String],
 });
 
-const nodeGroupModel = mongoose.model<INodesGroup & mongoose.Document>(process.env.NODE_GROUP_COLLECTION_NAME, nodeGroupSchema);
+const nodesGroupModel = mongoose.model<INodesGroup & mongoose.Document>(process.env.NODE_GROUP_COLLECTION_NAME , nodesGroupSchema);
 
-export default nodeGroupModel;
+export default nodesGroupModel;
