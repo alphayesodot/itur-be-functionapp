fe08d4772f31d2179dca0d4e57b0b34c86ef1a29
Yael <lustigyael10@gmail.com>
2021-02-14 10:26:52 +0200

    Initial commit

---
+++ getMalshabShared/utils.ts
@@ -0,0 +1,30 @@
+import * as mongoose from 'mongoose';
+import MalshabInterface from '../shared/malshab.interface';
+import { Permissionroles } from '../shared/types';
+import rolePermissions from '../shared/config';
+
+export const malshabFilter = (malshab: MalshabInterface & mongoose.Document<any>, allowedKeys: string[]): Partial<MalshabInterface> | undefined => {
+    const filtered = Object.keys(malshab.toJSON())
+        .filter((key) => allowedKeys.includes(key))
+        .reduce((obj, key) => {
+            // eslint-disable-next-line no-param-reassign
+            obj[key] = malshab[key];
+            return obj;
+        }, {});
+    if (Object.keys(filtered).length) return filtered;
+    return undefined;
+};
+
+export const gePermissions = (role: number): string[] => {
+    switch (role) {
+        case Permissionroles.Interviewer: {
+            return rolePermissions.interviewer;
+        }
+        case Permissionroles.check: {
+            return rolePermissions.check;
+        }
+        default: {
+            return [];
+        }
+    }
+};
