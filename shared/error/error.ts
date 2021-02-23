1020fdbfc0a7fcf61da1dd3c7324f93488715462
YonatanTal <yonatald@gmail.com>
2021-02-23 17:25:55 +0200

    update FunctionError
    Signed-off-by: YonatanTal <yonatald@gmail.com>

--- shared/error/error.ts
+++ shared/error/error.ts
@@ -7,8 +7,19 @@ export class ApplicationError extends Error {
     }
 }
 
-export class FunctionError extends ApplicationError {
-    constructor(message: string, functionName: string) {
+/**
+ * FunctionError is an extension of Error for Error in an Azure Function.
+ */
+export class FunctionError extends Error {
+    /**
+     * @param message - A message describing the error.
+     * @param functionName - The name of the azure function in which the error occurred.
+     * @param info - Additional info on the error.
+     */
+    constructor(message: string, functionName: string, info: string) {
         super(message);
+        if (!message) {
+            this.message = `Error in ${functionName} function: ${info}`;
+        }
     }
 }
